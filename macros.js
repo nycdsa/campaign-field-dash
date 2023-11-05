4b6f6f48736750229cadf2a73f2ce7c89f407563
SebastianCaceres <sebastiancaceres071@gmail.com>
2023-11-05 12:45:18 -0500

    added table formatting

--- macros.js
+++ macros.js
@@ -1,69 +1,103 @@
 /** @OnlyCurrentDoc */
 
-  var spreadsheet;
-  var parentFolder;
-  var contactData;
-  var surveyData;
-  var surveyFolder;
-  var contactFolder;
+var spreadsheet;
+var parentFolder;
+var contactData;
+var surveyData;
+var surveyFolder;
+var contactFolder_;
 
-
-
-function processSurvey(){
+function processSurvey() {
   setup();
   setupSheet("surveyDataTemp");
   setupFiles();
   processXLSFilesInZip(surveyFolder);
-  surveyData.getRange(2,1,surveyData.getLastRow()-1,surveyData.getLastColumn()).sort(3);
-  surveyData.getRange(2,1,surveyData.getLastRow()-1,surveyData.getLastColumn()).removeDuplicates([1,2,3,4,5,6,7,8]);
+  cleanupSheet(surveyData);
+}
 
-  
-
-};
-
-function processContact(){
+function processContact() {
   setup();
   setupSheet("contactDataTemp");
   setupFiles();
-  processXLSFilesInZip(contactFolder);
-  contactData.getRange(2,1,contactData.getLastRow()-1,contactData.getLastColumn()).sort(3);
-  contactData.getRange(2,1,contactData.getLastRow()-1,contactData.getLastColumn()).removeDuplicates([1,2,3,4,5,6,7,8,9,10]);
+  processXLSFilesInZip(contactFolder_);
+  cleanupSheet(contactData);
 }
 
-
-
+function cleanupSheet(sheet) {
+  var Array;
+  if (sheet.getName() == "surveyDataTemp") {
+    Array = [1, 2, 3, 4, 5, 6, 7, 8];
+  } else {
+    Array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  }
+  sheet.getRange(2, 1, sheet.getLastRow() - 1, sheet.getLastColumn()).sort(3);
+  sheet
+    .getRange(2, 1, sheet.getLastRow() - 1, sheet.getLastColumn())
+    .removeDuplicates(Array);
+}
 
 function setup() {
   spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
   parentFolder = DriveApp.getFileById(spreadsheet.getId()).getParents().next();
 
+  surveyHeader = [
+    [
+      "Voter File VANID",
+      "Contact Name",
+      "Date Canvassed",
+      "Survey Response",
+      "Survey Question Name",
+      "Survey Question Type",
+      "Canvassed By",
+      "Contact Type",
+      "Instance of VANID",
+      "Filename",
+      "Date/Time Added",
+    ],
+  ];
 
-  surveyHeader = [["Voter File VANID","Contact Name","Date Canvassed","Survey Response","Survey Question Name","Survey Question Type","Canvassed By","Contact Type","Instance of VANID","Filename","Date/Time Added"]];
-
-  contactHeader = [["Voter File VANID","Contact Name","Date Canvassed","Result","Contact Type","Canvassed By","Created By","Date Created","Input Type Name","MiniVAN Campaign","Instance of VANID","Filename","Date/Time Added"]]
+  contactHeader = [
+    [
+      "Voter File VANID",
+      "Contact Name",
+      "Date Canvassed",
+      "Result",
+      "Contact Type",
+      "Canvassed By",
+      "Created By",
+      "Date Created",
+      "Input Type Name",
+      "MiniVAN Campaign",
+      "Instance of VANID",
+      "Filename",
+      "Date/Time Added",
+    ],
+  ];
 }
 
-function setupSheet(sheetName){
-    if(sheetName == 'surveyDataTemp'){
-      surveyData = spreadsheet.getSheetByName(sheetName);
-      if (surveyData != null) {
-        surveyData.getDataRange().clear();
-      }else{
-        surveyData = spreadsheet.insertSheet(sheetName);
-      }
-      surveyData.getRange(1,1,1,surveyHeader[0].length).setValues(surveyHeader)      
-  } else{
+function setupSheet(sheetName) {
+  if (sheetName == "surveyDataTemp") {
+    surveyData = spreadsheet.getSheetByName(sheetName);
+    if (surveyData != null) {
+      surveyData.getDataRange().clear();
+    } else {
+      surveyData = spreadsheet.insertSheet(sheetName);
+    }
+    surveyData
+      .getRange(1, 1, 1, surveyHeader[0].length)
+      .setValues(surveyHeader);
+  } else {
     contactData = spreadsheet.getSheetByName(sheetName);
     if (contactData != null) {
       contactData.getDataRange().clear();
-    }else{
+    } else {
       contactData = spreadsheet.insertSheet(sheetName);
     }
-    contactData.getRange(1,1,1,contactHeader[0].length).setValues(contactHeader)       
+    contactData
+      .getRange(1, 1, 1, contactHeader[0].length)
+      .setValues(contactHeader);
   }
 }
-
-
 
 function setupFiles() {
   // Get the active spreadsheet and its parent folder
@@ -81,11 +115,13 @@ function setupFiles() {
       Logger.log(folderName + " folder already exists.");
     }
   });
-  surveyFolder = parentFolder.getFoldersByName("Survey Response Reports").next();
-  contactFolder = parentFolder.getFoldersByName("Contact History Reports").next();
-
-};
-
+  surveyFolder = parentFolder
+    .getFoldersByName("Survey Response Reports")
+    .next();
+  contactFolder_ = parentFolder
+    .getFoldersByName("Contact History Reports")
+    .next();
+}
 
 function processXLSFilesInZip(folder) {
   var files = folder.getFiles();
@@ -96,7 +132,7 @@ function processXLSFilesInZip(folder) {
 
     if (fileName.endsWith(".zip")) {
       var zipBlob = file.getBlob();
-    // Unzip the ZIP file
+      // Unzip the ZIP file
       var zip = Utilities.unzip(zipBlob);
 
       // Check for a single XLS file in the ZIP
@@ -105,8 +141,8 @@ function processXLSFilesInZip(folder) {
       });
 
       if (xlsFiles.length == 1) {
-        Logger.log("Processing: "+ fileName)
-        processXLSData(folder,xlsFiles[0])
+        Logger.log("Processing: " + fileName);
+        processXLSData(folder, xlsFiles[0]);
       } else if (xlsFiles.length == 0) {
         Logger.log("No XLS file found in the ZIP file: " + fileName);
         // Handle the case where no XLS file is found
@@ -116,50 +152,74 @@ function processXLSFilesInZip(folder) {
       }
     }
   }
-};
+}
 
-
-function processXLSData(folder,xlsBlob) {
+function processXLSData(folder, xlsBlob) {
   var newFile = {
-    title : xlsBlob.getName(),
-    parents: [{id: folder.getId()}] //  Added
+    title: xlsBlob.getName(),
+    parents: [{ id: folder.getId() }], //  Added
   };
   var file = Drive.Files.insert(newFile, xlsBlob, {
-    convert: true
+    convert: true,
   });
   var otherSpreadsheet = SpreadsheetApp.openById(file.id);
   var otherSheet = otherSpreadsheet.getSheets()[0]; // Change to the desired sheet name
-  var importedData = otherSheet.getRange(2,1,otherSheet.getLastRow()-1,otherSheet.getLastColumn()).getValues();
-
+  var importedData = otherSheet
+    .getRange(2, 1, otherSheet.getLastRow() - 1, otherSheet.getLastColumn())
+    .getValues();
 
   Drive.Files.remove(file.id); // Added // If this line is run, the original XLSX file is removed. So please be careful this.
 
   // Append the imported data to the existing or new sheet
 
-
-
-  if(folder.getName()=='Survey Response Reports'){
-    var startRow = surveyData.getLastRow() + 1
+  if (folder.getName() == "Survey Response Reports") {
+    var startRow = surveyData.getLastRow() + 1;
     var filename = Array(importedData.length).fill([xlsBlob.getName()]);
-    var dateTimeCol = Array(importedData.length).fill([Utilities.formatDate(new Date(), "America/New_York", "MM/dd/yyyy h:mm a")]);
+    var dateTimeCol = Array(importedData.length).fill([
+      Utilities.formatDate(new Date(), "America/New_York", "MM/dd/yyyy h:mm a"),
+    ]);
 
-    surveyData.getRange(startRow, 1, importedData.length, importedData[0].length).setValues(importedData);
+    surveyData
+      .getRange(startRow, 1, importedData.length, importedData[0].length)
+      .setValues(importedData);
 
-
-    surveyData.getRange(2,surveyData.getLastColumn()-2,importedData.length,1).setFormula("=COUNTIF(A$1:A2,A2)");;
-    surveyData.getRange(startRow,surveyData.getLastColumn()-1,importedData.length,1).setValues(filename);
-    surveyData.getRange(startRow,surveyData.getLastColumn(),importedData.length,1).setValues(dateTimeCol);
-  }else{
-    var startRow = contactData.getLastRow() + 1
+    surveyData
+      .getRange(2, surveyData.getLastColumn() - 2, importedData.length, 1)
+      .setFormula("=COUNTIF(A$1:A2,A2)");
+    surveyData
+      .getRange(
+        startRow,
+        surveyData.getLastColumn() - 1,
+        importedData.length,
+        1
+      )
+      .setValues(filename);
+    surveyData
+      .getRange(startRow, surveyData.getLastColumn(), importedData.length, 1)
+      .setValues(dateTimeCol);
+  } else {
+    var startRow = contactData.getLastRow() + 1;
     var filename = Array(importedData.length).fill([xlsBlob.getName()]);
-    var dateTimeCol = Array(importedData.length).fill([Utilities.formatDate(new Date(), "America/New_York", "MM/dd/yyyy h:mm a")]);
-    contactData.getRange(startRow, 1, importedData.length, importedData[0].length).setValues(importedData);
+    var dateTimeCol = Array(importedData.length).fill([
+      Utilities.formatDate(new Date(), "America/New_York", "MM/dd/yyyy h:mm a"),
+    ]);
+    contactData
+      .getRange(startRow, 1, importedData.length, importedData[0].length)
+      .setValues(importedData);
 
-
-    contactData.getRange(2,contactData.getLastColumn()-2,importedData.length,1).setFormula("=COUNTIF(A$1:A2,A2)");;
-    contactData.getRange(startRow,contactData.getLastColumn()-1,importedData.length,1).setValues(filename);
-    contactData.getRange(startRow,contactData.getLastColumn(),importedData.length,1).setValues(dateTimeCol);
-
+    contactData
+      .getRange(2, contactData.getLastColumn() - 2, importedData.length, 1)
+      .setFormula("=COUNTIF(A$1:A2,A2)");
+    contactData
+      .getRange(
+        startRow,
+        contactData.getLastColumn() - 1,
+        importedData.length,
+        1
+      )
+      .setValues(filename);
+    contactData
+      .getRange(startRow, contactData.getLastColumn(), importedData.length, 1)
+      .setValues(dateTimeCol);
   }
-
-};
+}
